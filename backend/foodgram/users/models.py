2d37323b959e0571dc9ca1a7af593490e56517d8
Maxim Bekurin <BekurinMD@yandex.ru>
2022-09-15 13:14:51 +0500

    models

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    '''модель пользователя'''
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    email = models.EmailField('Email',
+                              max_length=200,
+                              unique=True,)
+    first_name = models.CharField('Имя', max_length=150)
+    last_name = models.CharField('Фамилия', max_length=150)
+
+    class Meta:
+        ordering = ['id',]
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
