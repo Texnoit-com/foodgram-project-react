4799b23cd4bd299bb7a9965cc4ffcc5f46848f15
Maxim Bekurin <BekurinMD@yandex.ru>
2022-10-09 17:33:52 +0500

    a7

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='secretkey')
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = os.getenv('DEBUG', default='True')
 
